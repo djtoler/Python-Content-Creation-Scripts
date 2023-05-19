38c5b1e1715c3260fd15bf4b9fcfa4ec3055ed62
djtoler <84260551+djtoler@users.noreply.github.com>
2023-05-18 22:36:21 -0400

    removed bg from perspective image

--- src/twistImageLeftFront.py
+++ src/twistImageLeftFront.py
@@ -1,24 +1,30 @@
 import numpy as np
 import cv2
 
-image = cv2.imread("durk.png")  # Replace "your_image.jpg" with your actual image filename
+# Load your image
+image = cv2.imread("durk.png", cv2.IMREAD_UNCHANGED)  # Replace "durk.png" with your actual image filename
 
-# Define the source and destination points for the perspective transformation
+# Check if the image has an alpha channel, if not add one
+if image.shape[2] < 4:
+    # Convert the image from BGR to BGRA
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+
+# Define the source points for the perspective transformation
 src_points = np.float32([[0, 0], [image.shape[1] - 1, 0], [0, image.shape[0] - 1], [image.shape[1] - 1, image.shape[0] - 1]])
 
+# Define the destination points
 dst_points = np.float32([
     [0, 0], 
-    [image.shape[1] - 50, 50],  # Adjusted x-coordinate and y-coordinate for top-right
+    [image.shape[1] - 50, 0],  # Adjusted x-coordinate for top-right
     [0, image.shape[0] - 1], 
-    [image.shape[1] - 50, image.shape[0] - 130]  # Adjusted x-coordinate and y-coordinate for bottom-right
+    [image.shape[1] - 50, image.shape[0] - 100]  # Adjusted x-coordinate for bottom-right
 ])
-
 
 # Compute the perspective transformation matrix
 perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
 
 # Apply the perspective transformation to your image
-result = cv2.warpPerspective(image, perspective_matrix, (image.shape[1], image.shape[0]))
+result = cv2.warpPerspective(image, perspective_matrix, (image.shape[1], image.shape[0]), borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0, 0))
 
 # Save the result image to the current working directory
 cv2.imwrite("result.png", result)
